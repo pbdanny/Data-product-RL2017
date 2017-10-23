da0565006ce27e280fa24428d8b914b29b4e57f0
pbdanny <pb.danny@gmail.com>
2017-10-24 02:04:55 +0700

    Stage 2.2 plot salary-age by result

--- RLAnalysis.R
+++ RLAnalysis.R
@@ -14,6 +14,11 @@ rl2017$Occupation_Code <- as.character(rl2017$Occupation_Code)
 rl2017$Month <- as.character(rl2017$Month)
 
 # 1.2 Check outlier
+# Found extreme outliner Monthly_Salary = 18MMTHB/Mth
+# Directly Remove the record, since others varible
+# shown irreasonable,
+rl2017 <- rl2017[rl2017$Monthly_Salary < 5000000,]
+
 # Found AGE <- 0, use mice to imputate age
 library(mice)
 # MICE work for data = NA , then convert AGE<0 to NA
@@ -26,17 +31,36 @@ imp <- mice(df)
 # from imputate object
 rl2017$AGE <- complete(imp)$AGE
 
+# 1.3 Clear unused dataframe and save R Data file
+rm(list = c("df", "imp"))
+saveRDS(rl2017, file = "/Users/Danny/Share Win7/rl2017.RDA")
+
 ## 2) EDA check ----
-# 2.1 Univariate plot
+# Load Data
+rl2017 <- readRDS(file = "/Users/Danny/Share Win7/rl2017.RDA")
+
+# 2.1 Univariate plot 
 # Finalized by source code
-
 # Create contingency table count of Source Code, sorted
-tt <- sort(table(rl2017$Source_Code))
+tableSource <- sort(table(rl2017$Source_Code))
 # Barplot , create matrix 'barCenterX' : center of each bar in coordinate-x
-barCenterX <- barplot(tt)
+# Extend plot area with ylim + 20% of max y value
+barCenterX <- barplot(tableSource, ylim = c(0, 1.2*max(tableSource)))
 # Annotated plot wtih text at position x = barCenterX
-# y position = count of Source Code, lable = count of Source Code 
-text(x = barCenterX, tt, tt)
+# y position = count of Source Code, lable = count of Source Code
+# pos = 3, for label above y position specified
+text(x = barCenterX, y = tableSource, labels = tableSource, pos = 3)
+title(main = "Count of finalized by Source Code", sub = "data Jan. - Sep. 2017")
 # 2.2 Bivariate plot
-# 2.3 Geographical plot
 
+# From initail plot show outliner 
+# filter out Monthly_Salary > 10000
+library(ggplot2)
+
+ggplot(data = subset(rl2017, Monthly_Salary > 1000),
+       aes(x = AGE, 
+           y = log10(Monthly_Salary), 
+                     color = as.factor(Result))) +
+  geom_jitter(alpha = 0.5)
+
+# 2.3 Geographical plot
